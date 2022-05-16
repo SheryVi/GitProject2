a66c3bb393a875fe3c18f68471dcaeada6abd8d2
Vikki <v9858582209@gmail.com>
2022-05-17 02:27:43 +0300

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("I am Groot!");
+        System.out.println("I am Groot2!");
+        System.out.println("I am Groot3!");
 
-        
+
     }
 }
